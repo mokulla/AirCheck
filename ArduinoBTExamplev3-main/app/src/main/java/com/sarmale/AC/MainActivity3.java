812128e3ac4ab87017b9426f6822f59b58b834d4
maak2202 <124359334+maak2202@users.noreply.github.com>
2023-05-23 22:47:29 +0200

    Update MainActivity3.java

--- ArduinoBTExamplev3-main/app/src/main/java/com/sarmale/AC/MainActivity3.java
+++ ArduinoBTExamplev3-main/app/src/main/java/com/sarmale/AC/MainActivity3.java
@@ -1,3 +1,56 @@
+activity_main3.xml.        <?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    android:padding="16dp"
+    android:background="@drawable/bg2">
+
+    <TextView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Sample Text"
+        android:textSize="18sp" />
+    <LinearLayout
+
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+
+        android:orientation="horizontal">
+
+        <Button
+            android:id="@+id/back1"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:background="@drawable/rounded_button"
+
+            android:textSize="10dp"
+            android:text="Back "
+            android:onClick="penMain1"
+
+            />
+
+
+
+    </LinearLayout>
+    <LinearLayout
+        android:id="@+id/first_addressContainer"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:gravity="center"
+        android:orientation="vertical"
+        >
+
+        <ImageView
+            android:id="@+id/address"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:src="@drawable/graph2" />
+
+    </LinearLayout>
+
+</LinearLayout>
 package com.sarmale.AC;
 
 import androidx.appcompat.app.AppCompatActivity;
