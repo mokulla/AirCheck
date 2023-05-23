d6d4ea5f3f057ba47119b6f1010c63878648c286
mariankz <124358892+mariankz@users.noreply.github.com>
2023-05-23 19:51:30 +0200

    Update MainActivity.java

--- ArduinoBTExamplev3-main/app/src/main/java/com/sarmale/AC/MainActivity.java
+++ ArduinoBTExamplev3-main/app/src/main/java/com/sarmale/AC/MainActivity.java
@@ -4,21 +4,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
