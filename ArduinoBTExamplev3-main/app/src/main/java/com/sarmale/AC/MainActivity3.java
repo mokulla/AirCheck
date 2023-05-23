880cfd11121600035bd2766735e341d5b8942dc4
maak2202 <124359334+maak2202@users.noreply.github.com>
2023-05-23 22:50:19 +0200

    Update MainActivity3.java

--- ArduinoBTExamplev3-main/app/src/main/java/com/sarmale/AC/MainActivity3.java
+++ ArduinoBTExamplev3-main/app/src/main/java/com/sarmale/AC/MainActivity3.java
@@ -1,3 +1,25 @@
+package com.sarmale.AC;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+    public void openMain2(View view) {
+        startActivity(new Intent(this, MainActivity2.class));
+
+    }
+
+
+}
 <?xml version="1.0" encoding="utf-8"?>
 <LinearLayout
     xmlns:android="http://schemas.android.com/apk/res/android"
@@ -51,25 +73,3 @@
     </LinearLayout>
 
 </LinearLayout>
-package com.sarmale.AC;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
-public class MainActivity3 extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
-    }
-    public void openMain2(View view) {
-        startActivity(new Intent(this, MainActivity2.class));
-
-    }
-
-
-}
